e50d0f4344bd1751894a117ba10b504ccfc31899
王正一 <fsymbio@icloud.com>
2017-03-14 12:19:30 +0800

    创建命令行工具

---
+++ Package.swift
@@ -0,0 +1,9 @@
+import PackageDescription
+
+let package = Package(
+    name: "FengNiao",
+    dependencies: [
+        .Package(url: "https://github.com/jatoben/CommandLine.git", "3.0.0-pre1"),
+        .Package(url: "https://github.com/onevcat/Rainbow", majorVersion: 2)
+    ]
+)
---
+++ Sources/main.swift
@@ -0,0 +1,49 @@
+import Foundation
+import CommandLineKit
+import Rainbow
+
+let cli = CommandLineKit.CommandLine()
+
+let projectOption = StringOption(shortFlag: "p", longFlag: "project",
+                            helpMessage: "Path to the project.")
+let resourceExtensionOption = MultiStringOption(shortFlag: "r", longFlag: "resource-extensions",
+                          helpMessage: "Extensions to search.")
+
+let fileExtension = MultiStringOption(shortFlag: "f", longFlag: "file-extensions", helpMessage: "File extensions to search with")
+
+let help = BoolOption(shortFlag: "h", longFlag: "help",
+                      helpMessage: "Prints a help message.")
+
+cli.addOptions(projectOption, resourceExtensionOption, fileExtension, help)
+
+do {
+    try cli.parse()
+} catch {
+    cli.printUsage(error)
+    exit(EX_USAGE)
+}
+
+cli.formatOutput = { s, type in
+    var str: String
+    switch(type) {
+    case .error:
+        str = s.red.bold
+    case .optionFlag:
+        str = s.green
+    case .optionHelp:
+        str = s.blue
+    default:
+        str = s
+    }
+    
+    return cli.defaultFormat(s: str, type: type)
+}
+
+if help.value {
+    cli.printUsage()
+    exit(EX_OK)
+}
+
+let project = projectOption.value ?? "."
+let resourceExtensions = resourceExtensionOption.value ?? ["png", "jpg", "imageset"]
+let fileExtensions = fileExtension.value ?? ["swift", "m", "mm", "xib", "storyboard"]
